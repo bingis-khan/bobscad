fdd7b1adaca30365f71c916060abe0fb2d8cfd94
bingis_khan <52740618+bingis-khan@users.noreply.github.com>
2025-11-23 07:16:18 +0100

    fix arg for printing script

--- print-script.sh
+++ print-script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 mkdir -p stl
-for MODEL in $(cat "$0".scad | grep BOB_SCAD_PRINT | cut -d' ' -f3-); do
+for MODEL in $(cat "$1".scad | grep BOB_SCAD_PRINT | cut -d' ' -f3-); do
   openscad -D "print=true;${MODEL}()" -o "stl/${MODEL}.stl" keyboard.scad
 done
