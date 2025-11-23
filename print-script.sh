5aef6067db30b843e5aa96f31246ea5268d8f28d
bingis_khan <52740618+bingis-khan@users.noreply.github.com>
2025-11-23 07:18:03 +0100

    ye, i did not test shit

--- print-script.sh
+++ print-script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 mkdir -p stl
-for MODEL in $(cat "$1".scad | grep BOB_SCAD_PRINT | cut -d' ' -f3-); do
-  openscad -D "print=true;${MODEL}()" -o "stl/${MODEL}.stl" keyboard.scad
+for MODEL in $(cat "$1" | grep BOB_SCAD_PRINT | cut -d' ' -f3-); do
+  openscad -D "print=true;${MODEL}()" -o "stl/${MODEL}.stl" "$1"
 done
